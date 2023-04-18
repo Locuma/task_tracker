edcdef08caa550d0cf16fc7869bdb11e32eafdff
lonur <k.mitrofanov@orbis3.com>
2023-04-18 18:32:51 +0300

    start

---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\bootstrap5\Html;
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+<h1>hello world</h1>
+    <?= Html::encode($message) ?>
+</div>
